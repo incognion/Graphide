bf8776f17a048e83eef2d683f37f0ea5646b0771
Ashutosh Kumar <forjeeprep@gmail.com>
2024-09-22 20:27:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GraphManager from './components/GraphManager';
+
+function App() {
+    return (
+        <div className="App">
+            <GraphManager />
+        </div>
+    );
+}
+
+export default App;
