845f8d11ed24f3bf5a161391be87ba07424f229d
Ashutosh Kumar <forjeeprep@gmail.com>
2024-09-22 21:03:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ app.use(express.json());
 app.use(cors());
 
 const mongoUri = process.env.MONGO_URI;
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 
 const nodeSchema = new mongoose.Schema({ name: String });
 const edgeSchema = new mongoose.Schema({ from: String, to: String });
